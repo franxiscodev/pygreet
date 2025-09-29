09373d00752eca822caca3ee425b544624ac8e6a
FranxiscoDev <franxiscodev@gmail.com>
2025-09-29 23:01:09 +0200

    feat: la function resta en la rama resta

---
+++ operations.py
@@ -0,0 +1,17 @@
+"""
+Módulo de operaciones matemáticas
+"""
+
+
+def sub(a: float, b: float) -> float:
+    """
+    Resta dos números
+
+    Args:
+        a (float): Primer número
+        b (float): Segundo número
+
+    Returns:
+        float: Resultado de la resta
+    """
+    return a - b
